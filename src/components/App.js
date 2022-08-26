38a63282928ee2f3fa4196d4ccbd3bc144d961e5
Paul Rivera <juniorrivera81@yahoo.com>
2022-08-26 13:31:06 -0600

    commit tree a84c740dc3aa52ed45bfae242a7105fee8385ae2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-
+import About from "./About.js";
+import Home from "./Home.js";
+import NavBar from "./NavBar.js";
+import { username, city, image } from "../data/user.js";
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username={username} city={city} />
+      <About image={image} />
     </div>
   );
 }
